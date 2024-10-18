b8318cb81fbd17e957a12d9197f11699d524ae51
TheMaxium69 <mxm.tournier@gmail.com>
2024-10-18 13:48:30 +0200

    etape 1

---
+++ src/app/_interface/joke.interface.ts
@@ -0,0 +1,21 @@
+export interface JokeInterface {
+  categories: any[];
+  created_at: string;
+  icon_url: string;
+  id: string;
+  updated_at: string;
+  url: string;
+  value:string;
+}
+
+/*
+{
+    "categories": [],
+    "created_at": "2020-01-05 13:42:28.664997",
+    "icon_url": "https://api.chucknorris.io/img/avatar/chuck-norris.png",
+    "id": "BZfYEw4wTc2wjPNlQpX1_A",
+    "updated_at": "2020-01-05 13:42:28.664997",
+    "url": "https://api.chucknorris.io/jokes/BZfYEw4wTc2wjPNlQpX1_A",
+    "value": "Achmed the Dead Terrorist says \"I KILL YOU!\". Chuck Norris kills you without telling you anything."
+}
+*/
---
+++ src/app/_service/joke.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JokeService {
+
+  constructor() { }
+
+  getJokeRandom(){
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+  /* CRUD DEFAULT */
+
+  getJokeAll(){}
+
+  getJokeOne(){}
+
+  createJoke(){}
+
+  deleteJoke(){}
+
+  editJoke(){}
+
+}
