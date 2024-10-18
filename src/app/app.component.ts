abc525321c1adce72e7e83c376006db0559c39e4
TheMaxium69 <mxm.tournier@gmail.com>
2024-10-18 12:31:33 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from "./other/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ChuckR6';
+export class AppComponent implements OnInit {
+
+  /*
+  *
+  * VARIABLE GLOBAL
+  *
+  * */
+
+  APP_ENV:string = "DEV"; /* PROD or DEV*/
+  API_URL:string = "";
+
+  ngOnInit():void {
+
+    /* GESTION DE L'API */
+    if (this.APP_ENV == "DEV") {
+      this.API_URL = "http://localhost:8080/api";
+    } else if(this.APP_ENV == "PROD") {
+      this.API_URL = "http://localhost:8080/api";
+    } else {
+      this.API_URL = "http://localhost:8080/api";
+    }
+
+  }
+
+  /*
+  *
+  * FUNCTION GLOBAL
+  *
+  * */
+
+  createCors(){
+
+  }
+
+
 }
